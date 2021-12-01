db8d9b291121e23456ea2543dcffb28e2a1360a4
KarinShoji <36564217+sika0115@users.noreply.github.com>
2021-12-01 19:05:07 +0900

    ReactのPropsのところまで

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import React from "react"; //Reactのお作法的なもの、React17でJSXを使用するだけの場合import必須でなくなった
+import ReactDom from "react-dom"; //react-domのインポート・・・これ後でReactDomがなんなのか調べる
+import App from "./App.jsx"; //他のファイルで画面のレンダリング部分を作成、コンポーネント化してindex.jsで利用
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+//Appを画面に反映する ReactDomライブラリの中のrender関数を使う
+//1個目の引数：Appをコンポーネントとしてレンダリングするという意味でタグのように書く
+//2個目の引数：htmlのどこに反映していくかを書く→jsでindex.htmlのid rootを指定
+ReactDom.render(<App />, document.getElementById("root"));
