169f6a8f43caf3593b55c2e29e9a6b7e557ed5e7
KarinShoji <36564217+sika0115@users.noreply.github.com>
2021-12-02 16:47:50 +0900

    Stateの学習まで

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  console.log("開始");
+  //State
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  //numが3の倍数の時にfaceShowFlagをtrueにする
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        //既にtrueの場合は更新しない
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        //既にfalseなら更新しない
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">お元気ですか</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>(+_+)</p>}
     </>
   );
 };
